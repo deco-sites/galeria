67cf884d9dd3f509e1f233cc8be32c2e3c3d07cf
Henrique Verri <henriqueverri41@gmail.com>
2024-11-18 17:46:42 -0300

    ajustes de animação next page

--- components/ui/NextPage/index.tsx
+++ components/ui/NextPage/index.tsx
@@ -12,7 +12,142 @@ function NextPage({
 }: JSX.IntrinsicElements["div"] & Props) {
   return (
     <>
-      <div id="nextPage" {...props}>
+    
+    <style>
+        {`
+        .anim-fadeinup {
+            opacity: 0;
+            transform: translateY(100px);
+            transition: opacity 2.5s ease-out, transform 2.5s ease-out;
+        }
+        
+        .anim-fadeinup.visible {
+            opacity: 1;
+            transform: translateY(0);
+        }
+            .tt-page-nav {
+    position: relative;
+    max-width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+}
+            .tt-pn-link {
+    position: relative;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    overflow: hidden;
+}
+    @media (min-width: 1025px) {
+    .tt-pn-link:hover .tt-pn-hover-title {
+        transform: translate3d(-50%, 0, 0);
+        transition-delay: 0.1s;
+    }
+}
+    @media (min-width: 1025px) {
+    .tt-pn-link:hover .tt-pn-title {
+        transform: translate3d(-50%, -100%, 0);
+        transition-delay: 0.1s;
+    }
+}
+    .font-h2 {
+    font-family: Helvetica, sans-serif;
+    /* font-size: 32px !important; */
+    font-size: 4.5vw !important;
+    font-weight: 700 !important;
+    line-height: 1 !important;
+    letter-spacing: -1.36px !important;
+}
+    .tt-pn-title {
+    position: relative;
+    display: block;
+    left: 50%;
+    font-size: clamp(42px, 6vw, 110px);
+    font-weight: bold;
+    font-weight: 500;
+    color: #FFF;
+    line-height: 1.6;
+    opacity: .4;
+    white-space: nowrap;
+    transform: translate3d(-50%, 0, 0);
+    transition: transform .6s cubic-bezier(0.51, 0.57, 0.17, 1);
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+    .tt-pn-hover-title {
+    position: absolute;
+    left: 50%;
+    font-size: clamp(36px, 6vw, 110px);
+    font-weight: bold;
+    font-weight: 500;
+    color: #FFF;
+    line-height: 1.6;
+    white-space: nowrap;
+    transform: translate3d(-50%, 100%, 0);
+    transition: transform .6s cubic-bezier(0.51, 0.57, 0.17, 1);
+}
+    .tt-page-nav .tt-pn-image {
+    display: none;
+}
+    .tt-pn-image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+}
+    @media (min-width: 1025px) {
+    .tt-page-nav .tt-pn-link:hover .tt-pn-hover-title span {
+        animation: marquee 3s linear infinite;
+        animation-play-state: running;
+    }
+}
+    @media (min-width: 1025px) {
+    .tt-page-nav .tt-pn-hover-title span {
+        display: inline-flex;
+        align-items: center;
+        padding-right: 5vw;
+        animation: marquee 3s linear infinite;
+        animation-play-state: paused;
+    }
+}
+    /* Estilo do magic-cursor */
+#magic-cursor.tt-pn-hover-on {
+    transition: opacity 0.3s, width 0.3s, height 0.3s;
+    opacity: 1;
+    width: 20vw;
+    height: 20vw;
+}
+
+/* Transições de elementos dentro do magic-cursor */
+#magic-cursor {
+    position: absolute;
+    display: block;
+    width: 10px;
+    height: 10px;
+    pointer-events: none;
+    z-index: 99999;
+    opacity: 0;
+}
+    
+#ball {
+	position: fixed;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	color: #FFF;
+	pointer-events: none;
+	border-style: solid;
+	/*border-color: #FFF;*/
+	border-radius: 50%;
+	/* Note: border width handled through JS */
+}
+
+        `}
+      </style>
+      {/* <div id="nextPage" {...props} className="tt-page-nav tt-pn-scroll">
         <a href={url} class="container !py-[150px] relative block bg-black">
           <div id="nextPageImage" className="hidden w-[200px] h-[200px]">
               <Image
@@ -29,6 +164,30 @@ function NextPage({
             </p>
           </div>
         </a>
+      </div> */}
+      <div className="py-10">
+      <div id="magic-cursor">
+				<div id="ball"></div>
+			</div>
+        <div className="tt-page-nav tt-pn-scroll">
+          <a href="pra-voce-chegar-la.html" className="tt-pn-link anim-fadeinup">
+            <div className="tt-pn-title text-uppercase font-h2">Itaú</div>
+            <div className="tt-pn-hover-title text-uppercase font-h2">UniclassName</div>
+          </a> 
+
+          <div className="tt-pn-subtitle anim-fadeinup">Próximo Trabalho</div>
+
+          
+          <div className="tt-pn-image">
+          <Image
+                  src={image || ""}
+                  alt={text}
+                  className="w-full h-full object-cover rounded-full"
+                  loading="lazy"
+                  decoding="async"
+              />
+          </div>
+        </div>
       </div>
       <Script 
         text={text} 
--- components/ui/NextPage/script.tsx
+++ components/ui/NextPage/script.tsx
@@ -11,29 +11,158 @@ const setup = ({ text, hoverText }: Props) => {
     const nextPageImage = document.getElementById('nextPageImage');
     const nextPageCaption = document.getElementById('nextPageCaption');
 
-    nextPage?.addEventListener('mouseenter', function(e) {
+    // nextPage?.addEventListener('mouseenter', function(e) {
       
-      nextPageImage?.classList.remove('hidden')
+    //   nextPageImage?.classList.remove('hidden')
 
-      if(nextPageCaption){
-        nextPageCaption.textContent = hoverText
-        nextPageCaption.classList.add('text-white')
-        nextPageCaption.classList.add('animate-marquee')
+    //   if(nextPageCaption){
+    //     nextPageCaption.textContent = hoverText
+    //     nextPageCaption.classList.add('text-white')
+    //     nextPageCaption.classList.add('animate-marquee')
+    //   }
+
+    // })
+
+    // nextPage?.addEventListener('mouseleave', function(e) {
+
+    //   nextPageImage?.classList.add('hidden')
+
+    //   if(nextPageCaption){
+    //     nextPageCaption.textContent = text
+    //     nextPageCaption.classList.remove('text-white')
+    //     nextPageCaption.classList.remove('animate-marquee')
+    //   }
+
+    // })
+    
+    // Verificar se o elemento com a classe 'tt-page-nav' possui a classe 'tt-pn-scroll'
+    if (document.querySelector(".tt-page-nav").classList.contains("tt-pn-scroll")) {      
+      console.log('aqui');
+        
+      // Seleciona todos os elementos com a classe 'tt-pn-hover-title' dentro de 'tt-page-nav'
+      document.querySelectorAll(".tt-page-nav .tt-pn-hover-title").forEach(function(element) {
+      console.log('aqui 1');
+          
+          // Envolvendo o conteúdo interno do elemento com uma tag <span>
+          var span = document.createElement('span');
+          while (element.firstChild) {
+              span.appendChild(element.firstChild);
+          }
+          element.appendChild(span);
+
+          // Clonando o título hover 7 vezes e inserindo após o original
+          var pnHoverTitle = element.querySelector("span");
+          for (var i = 0; i < 7; i++) {
+              var clonedTitle = pnHoverTitle.cloneNode(true);
+              pnHoverTitle.parentNode.insertBefore(clonedTitle, pnHoverTitle.nextSibling);
+          }
+      });
+
+    } else {
+      
+      // Caso a condição anterior não seja satisfeita, verifica se a largura do 'tt-pn-hover-title' é maior que a do pai
+      document.querySelectorAll(".tt-page-nav .tt-pn-hover-title").forEach(function(element) {
+          
+          if (element.offsetWidth > element.parentElement.offsetWidth) {
+              
+              // Envolvendo o conteúdo interno do elemento com uma tag <span>
+              var span = document.createElement('span');
+              while (element.firstChild) {
+                  span.appendChild(element.firstChild);
+              }
+              element.appendChild(span);
+
+              // Clonando o título hover 7 vezes e inserindo após o original
+              var pnHoverTitle = element.querySelector("span");
+              for (var i = 0; i < 7; i++) {
+                  var clonedTitle = pnHoverTitle.cloneNode(true);
+                  pnHoverTitle.parentNode.insertBefore(clonedTitle, pnHoverTitle.nextSibling);
+              }
+          }
+      });
+    }
+    
+    // Função para verificar se o elemento está visível na tela
+    function isElementInViewport(el) {
+      const rect = el.getBoundingClientRect();
+      return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+    }
+
+    // Adiciona a classe 'visible' aos elementos quando eles entram na tela
+    function handleScroll() {
+      const elements = document.querySelectorAll('.anim-fadeinup');
+      
+      elements.forEach(function(element) {
+          if (isElementInViewport(element)) {
+              element.classList.add('visible');
+          }
+      });
+    }
+
+    // Chama a função `handleScroll` no evento de scroll e ao carregar a página
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('load', handleScroll);
+
+    // Chama a função uma vez ao carregar a página para aplicar em elementos que já estão visíveis
+    handleScroll();
+
+
+    document.querySelectorAll('.tt-page-nav').forEach(function(navElement) {
+      // Verifica se existe a imagem dentro da navegação
+      const imageElement = navElement.querySelector('.tt-pn-image');
+      const linkElement = navElement.querySelector('.tt-pn-link');
+  
+      if (imageElement) {
+          const ball = document.getElementById('magic-cursor');
+          let ballWidth = ball.offsetWidth;
+          let ballHeight = ball.offsetHeight;
+          let ballOpacity = ball.style.opacity;
+  
+          linkElement.addEventListener('mouseenter', function() {
+              ball.classList.add('tt-pn-hover-on');
+              
+              // Move a imagem para dentro do cursor
+              ball.appendChild(imageElement);
+              
+              // Alterando a animação de tamanho e opacidade
+              ball.style.width = '20vw';
+              ball.style.height = '20vw';
+              ball.style.opacity = '1';
+  
+              // Reproduz os vídeos
+              const videos = ball.querySelectorAll('.tt-pn-image video');
+              videos.forEach(function(video) {
+                  video.play();
+              });
+          });
+  
+          linkElement.addEventListener('mouseleave', function() {
+              ball.classList.remove('tt-pn-hover-on');
+  
+              // Retorna a imagem para o seu lugar original
+              navElement.appendChild(imageElement);
+  
+              // Restaura o tamanho e a opacidade do cursor
+              ball.style.width = `${ballWidth}px`;
+              ball.style.height = `${ballHeight}px`;
+              ball.style.opacity = ballOpacity;
+  
+              // Pausa os vídeos
+              const videos = navElement.querySelectorAll('.tt-pn-image video');
+              videos.forEach(function(video) {
+                  video.pause();
+              });
+          });
+  
+          // Adiciona a classe para não esconder o cursor
+          linkElement.classList.add('not-hide-cursor');
+      } else {
+          // Remove a classe se a imagem não existir
+          linkElement.classList.remove('not-hide-cursor');
       }
+  });
+  
 
-    })
-
-    nextPage?.addEventListener('mouseleave', function(e) {
-
-      nextPageImage?.classList.add('hidden')
-
-      if(nextPageCaption){
-        nextPageCaption.textContent = text
-        nextPageCaption.classList.remove('text-white')
-        nextPageCaption.classList.remove('animate-marquee')
-      }
-
-    })
 
 }
 
